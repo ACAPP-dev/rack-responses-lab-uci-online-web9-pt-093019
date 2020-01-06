735602cb68c38cc6bc5a837203e19211a5c1db8e
Andrew Capp <acapp909@gmail.com>
2020-01-06 21:52:41 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -6,6 +6,11 @@ class Application
     def call(env)
       resp = Rack::Response.new
 
-      if time.now
+      if Time.now.to_i < 12
+        resp.write "Good Morning"
+      else
+        resp.write "Good Afternoon"
+      end
 
+      resp.finish
 end
